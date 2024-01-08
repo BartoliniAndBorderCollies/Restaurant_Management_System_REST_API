721c5cf159510696d5fd640f49d3a64e54634f29
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-08 13:53:40 +0100

    initial commit

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/RestaurantManagementSystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Restaurant_Management_System_REST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementSystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementSystemRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Restaurant_Management_System_REST_API/RestaurantManagementSystemRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Restaurant_Management_System_REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
