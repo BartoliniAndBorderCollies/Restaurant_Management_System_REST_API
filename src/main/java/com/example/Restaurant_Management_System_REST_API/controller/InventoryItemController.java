d4ea88c3b5e5a42118d2b047dd19525fac18f274
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-11 17:51:54 +0100

    add controllers

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/controller/InventoryItemController.java
@@ -0,0 +1,9 @@
+package com.example.Restaurant_Management_System_REST_API.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryItemController {
+}
