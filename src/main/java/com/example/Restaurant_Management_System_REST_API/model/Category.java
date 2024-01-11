d2cf4564641c16515b6c7e5cd7449ac1b0c81da9
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-11 17:54:15 +0100

    add enum classes to the model

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/Category.java
@@ -0,0 +1,9 @@
+package com.example.Restaurant_Management_System_REST_API.model;
+
+public enum Category {
+
+    BEVERAGE,
+    MAIN_DISH,
+    SNACKS,
+    STARTERS
+}
---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.example.Restaurant_Management_System_REST_API.model;
+
+public enum OrderStatus {
+
+    PENDING,
+    PREPARING,
+    DONE,
+    CANCELLED,
+    PAID,
+    NOT_PAID
+}
