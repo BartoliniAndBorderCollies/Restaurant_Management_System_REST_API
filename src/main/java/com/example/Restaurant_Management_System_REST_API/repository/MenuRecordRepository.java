e01479d2b771f93c1b1cd1f88309ba10c03704c2
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-15 11:53:11 +0100

    delete empty classes

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/MenuRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Restaurant_Management_System_REST_API.repository;
-
-import com.example.Restaurant_Management_System_REST_API.model.entity.MenuRecord;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuRecordRepository extends CrudRepository<MenuRecord, Long> {
-}
