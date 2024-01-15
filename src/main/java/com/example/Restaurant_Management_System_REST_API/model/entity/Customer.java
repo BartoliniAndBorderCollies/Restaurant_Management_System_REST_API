8a70697f859be0abdad5df6ac3732b200430e3f5
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-15 14:39:04 +0100

    refactor of Customer.java -> instead of GrantedAuthority I use SimpleGrantedAuthority

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/Customer.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/Customer.java
@@ -6,11 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Entity
@@ -29,15 +31,14 @@ public class Customer implements UserDetails {
     //Below are fields for security issues
     private String password;
     private String emailAddress; //TODO: delete when abstract class with contact details will be implemented
-    @ManyToMany
-    @JoinTable(name = "user_authority",
-            joinColumns = @JoinColumn(name = "customer_id"),
-            inverseJoinColumns = @JoinColumn(name = "authority_id"))
-    private Set<Authority> authorities;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
     @Override
