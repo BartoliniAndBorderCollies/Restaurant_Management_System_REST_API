bc9a98f7ab91c00fe0cfca445d544b02087ff9ef
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-17 12:08:41 +0100

    add GlobalCustomExceptionHandler with handling NotFoundInDbException with appropriate message to the client

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/controller/advice/GlobalCustomExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.Restaurant_Management_System_REST_API.controller.advice;
+
+import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalCustomExceptionHandler {
+
+    @ExceptionHandler(NotFoundInDatabaseException.class)
+    public ResponseEntity<?> handleNotFoundInDatabaseException (NotFoundInDatabaseException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
