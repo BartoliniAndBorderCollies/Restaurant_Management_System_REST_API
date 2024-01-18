fc1efbb4e285871c858b5948d03c1ece8547d6e2
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-18 12:52:30 +0100

    refactor of CustomerDetailsService to return the whole Customer object or throw a message

--- src/main/java/com/example/Restaurant_Management_System_REST_API/service/CustomerDetailsService.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/service/CustomerDetailsService.java
@@ -1,6 +1,5 @@
 package com.example.Restaurant_Management_System_REST_API.service;
 
-import com.example.Restaurant_Management_System_REST_API.model.entity.Customer;
 import com.example.Restaurant_Management_System_REST_API.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,11 +16,8 @@ public class CustomerDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        Customer customer = customerRepository.findByEmailAddress(email).orElseThrow(() ->
-                new UsernameNotFoundException("Username with "
-                        + email + " was not found!"));
-
-        return new org.springframework.security.core.userdetails.User(customer.getUsername(), customer.getPassword(), customer.getAuthorities());
+        return customerRepository.findByEmailAddress(email).orElseThrow(() -> new UsernameNotFoundException("Username with "
+                + email + " was not found!"));
     }
 }
 
