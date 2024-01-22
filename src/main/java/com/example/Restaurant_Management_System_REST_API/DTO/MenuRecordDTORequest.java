331a4f923efa1524f01a95ae1cf3a0327c754103
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-22 11:52:34 +0100

    add MenuRecordDTORequest and MenuRecordDTOResponse

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/MenuRecordDTORequest.java
@@ -0,0 +1,22 @@
+package com.example.Restaurant_Management_System_REST_API.DTO;
+
+import com.example.Restaurant_Management_System_REST_API.model.CatalogItem;
+import com.example.Restaurant_Management_System_REST_API.model.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuRecordDTORequest extends CatalogItem {
+
+    private Long id;
+    private Set<String> ingredients;
+    private Category category;
+    private Boolean isAvailable;
+}
