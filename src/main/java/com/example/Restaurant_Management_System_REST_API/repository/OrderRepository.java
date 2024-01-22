25ab193af6198a03f18ec1c98af5cfc8ddd10298
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-22 11:05:12 +0100

    add extension to OrderRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/OrderRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.example.Restaurant_Management_System_REST_API.repository;
 
+import com.example.Restaurant_Management_System_REST_API.model.entity.Order;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 }
