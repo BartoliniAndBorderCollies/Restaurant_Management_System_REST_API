438d73928fcde25409e9eb0cdd6ac54d0b8a07f2
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-22 11:12:34 +0100

    add bean for MvcRequestMatcher.Builder -> without it app does not start

--- src/main/java/com/example/Restaurant_Management_System_REST_API/configuration/SecurityConfig.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/configuration/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @Configuration
@@ -34,6 +35,11 @@ public class SecurityConfig {
                         .permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
     }
 
     @Bean
