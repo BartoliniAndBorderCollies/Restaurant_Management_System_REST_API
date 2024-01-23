6ebdf4e1cd86a7ad24b0869a891f159b0e20093b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-23 09:20:03 +0100

    add validations to MenuRecord fields

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/MenuRecord.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/MenuRecord.java
@@ -3,6 +3,8 @@ package com.example.Restaurant_Management_System_REST_API.model.entity;
 import com.example.Restaurant_Management_System_REST_API.model.Category;
 import com.example.Restaurant_Management_System_REST_API.model.CatalogItem;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,8 +18,14 @@ public class MenuRecord extends CatalogItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    @NotEmpty(message = "Must have at least one ingredient!")
+    @Column(nullable = false)
     private Set<String> ingredients;
+    @NotNull (message = "Category is missing!")
+    @Column(nullable = false)
     private Category category;
+    @NotNull(message = "Is available is missing!")
+    @Column(nullable = false)
     private Boolean isAvailable; //I use object to be able to hold null values (for updating process, I want to update
     //just fields which hold values, if some are skipped they should not be changed on db. if it was a primitive data type
     //of boolean then it would change to false as default
