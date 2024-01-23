472279999d0c91d92d89c9acbe472736c81b58ed
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-23 09:21:02 +0100

    add exception handling method to GlobalCustomExceptionHandler

--- src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/advice/GlobalCustomExceptionHandler.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/advice/GlobalCustomExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.example.Restaurant_Management_System_REST_API.rest.controller.advice;
 
 import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
+import org.hibernate.PropertyValueException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,5 +13,11 @@ public class GlobalCustomExceptionHandler {
     @ExceptionHandler(NotFoundInDatabaseException.class)
     public ResponseEntity<?> handleNotFoundInDatabaseException (NotFoundInDatabaseException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PropertyValueException.class)
+    public ResponseEntity<String> handlePropertyValueException (PropertyValueException ex) {
+        return new ResponseEntity<>("Something is missing! You didn't specified a required field in your request: "
+                + ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
