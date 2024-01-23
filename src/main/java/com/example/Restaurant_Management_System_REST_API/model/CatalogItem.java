efe182ca59e031d9210d555f6a87cc67b8797adb
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-23 09:20:16 +0100

    add validations to CatalogItem fields

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/CatalogItem.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/CatalogItem.java
@@ -1,6 +1,10 @@
 package com.example.Restaurant_Management_System_REST_API.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +13,13 @@ import lombok.Setter;
 @MappedSuperclass
 public abstract class CatalogItem {
 
+    @NotBlank(message = "Name is missing!")
+    @Column(nullable = false)
     private String name;
     private String description;
+    @PositiveOrZero(message = "Must be zero or more!")
+    @NotNull(message = "Price is missing!")
+    @Column(nullable = false)
     private Double price; //I use object to be able to hold null values (for updating process, I want to update
     //just fields which hold values, if some are skipped they should not be changed on db. if it was a primitive data type
     //of double then it would change to 0.0 if not provided other value in put request
