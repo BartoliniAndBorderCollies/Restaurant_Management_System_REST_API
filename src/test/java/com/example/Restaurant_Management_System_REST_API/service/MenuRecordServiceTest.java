8b84e0fb0d89e015b94d71f482a7cd4df1839b05
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-24 10:16:25 +0100

    create @Nested test class for create()

--- src/test/java/com/example/Restaurant_Management_System_REST_API/service/MenuRecordServiceTest.java
+++ src/test/java/com/example/Restaurant_Management_System_REST_API/service/MenuRecordServiceTest.java
@@ -2,15 +2,16 @@ package com.example.Restaurant_Management_System_REST_API.service;
 
 import com.example.Restaurant_Management_System_REST_API.DTO.MenuRecordDTORequest;
 import com.example.Restaurant_Management_System_REST_API.DTO.MenuRecordDTOResponse;
+import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
 import com.example.Restaurant_Management_System_REST_API.model.Category;
 import com.example.Restaurant_Management_System_REST_API.model.entity.MenuRecord;
 import com.example.Restaurant_Management_System_REST_API.repository.MenuRecordRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -24,61 +25,68 @@ class MenuRecordServiceTest {
     private MenuRecordDTORequest menuRecordDTORequest;
     private MenuRecord menuRecord;
     private MenuRecordDTOResponse menuRecordDTOResponse;
+    private Set<String> ingredients;
 
     @BeforeEach
-    public void setUp() {
-        Set<String> ingredients = new HashSet<>();
+    public void generalSetUp() {
+        menuRecordRepository = mock(MenuRecordRepository.class);
+        modelMapper = mock(ModelMapper.class);
+        menuRecordService = new MenuRecordService(menuRecordRepository, modelMapper);
+
+        ingredients = new HashSet<>();
         ingredients.add("water");
         ingredients.add("hops");
         ingredients.add("barley");
-
-        menuRecordDTORequest = new MenuRecordDTORequest(1L, ingredients, Category.BEVERAGE, true);
-        menuRecord = new MenuRecord(1L, ingredients, Category.BEVERAGE, true);
-        menuRecordDTOResponse = new MenuRecordDTOResponse(1L, ingredients, Category.BEVERAGE, false);
-
-        menuRecordRepository = mock(MenuRecordRepository.class);
-        modelMapper = mock(ModelMapper.class);
-
-        menuRecordService = new MenuRecordService(menuRecordRepository, modelMapper);
     }
 
-    @Test
-    public void create_ShouldInteractWithDependenciesCorrectly_WhenMenuRecordDTORequestIsGiven() {
-        //Arrange
-        when(modelMapper.map(menuRecordDTORequest, MenuRecord.class)).thenReturn(menuRecord);
-        when(menuRecordRepository.save(menuRecord)).thenReturn(menuRecord);
-        when(modelMapper.map(menuRecord, MenuRecordDTOResponse.class)).thenReturn(menuRecordDTOResponse);
 
-        //Act
-        MenuRecordDTOResponse actual = menuRecordService.create(menuRecordDTORequest);
+    @Nested
+    class nestedClassForCreateMethods {
 
-        //Assert
-        assertEquals(menuRecordDTOResponse, actual);
-    }
+        @BeforeEach
+        public void setUpForCreateMethods() {
+            menuRecordDTORequest = new MenuRecordDTORequest(1L, ingredients, Category.BEVERAGE, true);
+            menuRecord = new MenuRecord(1L, ingredients, Category.BEVERAGE, true);
+            menuRecordDTOResponse = new MenuRecordDTOResponse(1L, ingredients, Category.BEVERAGE, false);
+        }
 
-    @Test
-    public void create_ShouldReturnTypeMenuRecordDTOResponse_WhenMenuRecordDTORequestIsGiven() {
-        //Arrange - takes from @BeforeEach
-        when(modelMapper.map(menuRecordDTORequest, MenuRecord.class)).thenReturn(menuRecord);
-        when(menuRecordRepository.save(menuRecord)).thenReturn(menuRecord);
-        when(modelMapper.map(menuRecord, MenuRecordDTOResponse.class)).thenReturn(menuRecordDTOResponse);
+        @Test
+        public void create_ShouldInteractWithDependenciesCorrectly_WhenMenuRecordDTORequestIsGiven() {
+            //Arrange
+            when(modelMapper.map(menuRecordDTORequest, MenuRecord.class)).thenReturn(menuRecord);
+            when(menuRecordRepository.save(menuRecord)).thenReturn(menuRecord);
+            when(modelMapper.map(menuRecord, MenuRecordDTOResponse.class)).thenReturn(menuRecordDTOResponse);
 
-        //Act
-        MenuRecordDTOResponse actual = menuRecordService.create(menuRecordDTORequest);
+            //Act
+            MenuRecordDTOResponse actual = menuRecordService.create(menuRecordDTORequest);
 
-        //Assert
-        assertEquals(menuRecordDTOResponse, actual);
-    }
+            //Assert
+            assertEquals(menuRecordDTOResponse, actual);
+        }
 
-    @Test
-    public void create_ShouldCallExactlyOnceOnMenuRecordRepo_WhenMenuRecordDTORequestIsGiven() {
-        //Arrange - takes from @BeforeEach
-        when(modelMapper.map(menuRecordDTORequest, MenuRecord.class)).thenReturn(menuRecord);
-        when(menuRecordRepository.save(menuRecord)).thenReturn(menuRecord);
-        when(modelMapper.map(menuRecord, MenuRecordDTOResponse.class)).thenReturn(menuRecordDTOResponse);
+        @Test
+        public void create_ShouldReturnTypeMenuRecordDTOResponse_WhenMenuRecordDTORequestIsGiven() {
+            //Arrange - takes from @BeforeEach
+            when(modelMapper.map(menuRecordDTORequest, MenuRecord.class)).thenReturn(menuRecord);
+            when(menuRecordRepository.save(menuRecord)).thenReturn(menuRecord);
+            when(modelMapper.map(menuRecord, MenuRecordDTOResponse.class)).thenReturn(menuRecordDTOResponse);
 
-        //Act
-        menuRecordService.create(menuRecordDTORequest);
+            //Act
+            MenuRecordDTOResponse actual = menuRecordService.create(menuRecordDTORequest);
+
+            //Assert
+            assertEquals(menuRecordDTOResponse, actual);
+        }
+
+        @Test
+        public void create_ShouldCallExactlyOnceOnMenuRecordRepo_WhenMenuRecordDTORequestIsGiven() {
+            //Arrange - takes from @BeforeEach
+            when(modelMapper.map(menuRecordDTORequest, MenuRecord.class)).thenReturn(menuRecord);
+            when(menuRecordRepository.save(menuRecord)).thenReturn(menuRecord);
+            when(modelMapper.map(menuRecord, MenuRecordDTOResponse.class)).thenReturn(menuRecordDTOResponse);
+
+            //Act
+            menuRecordService.create(menuRecordDTORequest);
 
         //Assert
         verify(menuRecordRepository, times(1)).save(menuRecord);
