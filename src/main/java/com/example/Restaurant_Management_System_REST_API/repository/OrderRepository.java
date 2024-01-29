2a133f3a07f64454720ff10fd0115eb49f94e63f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-29 12:29:25 +0100

    delete of unneeded repo

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Restaurant_Management_System_REST_API.repository;
-
-import com.example.Restaurant_Management_System_REST_API.model.entity.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
