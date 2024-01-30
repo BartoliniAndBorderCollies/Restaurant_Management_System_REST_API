94d013caaa03cd79720d40c62c2a14b905794836
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-01-30 12:47:03 +0100

    add CustomerDTOs classes

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/CustomerDTOs/CustomerDTOResponse.java
@@ -0,0 +1,30 @@
+package com.example.Restaurant_Management_System_REST_API.DTO.CustomerDTOs;
+
+import com.example.Restaurant_Management_System_REST_API.model.ContactDetails;
+import com.example.Restaurant_Management_System_REST_API.model.entity.Authority;
+import com.example.Restaurant_Management_System_REST_API.model.entity.Reservation;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+public class CustomerDTOResponse {
+
+
+    private Long id;
+    private LocalDateTime creationTime;
+    private Reservation reservation;
+    private ContactDetails contactDetails;
+
+    //Below are fields for security issues
+    private String password;
+    private Boolean accountNonExpired;//to avoid hardcoding I established these fields
+    private Boolean accountNonLocked;
+    private Boolean credentialsNonExpired;
+    private Boolean enabled;
+    private String emailAddress;
+    private Set<Authority> authorities;
+}
