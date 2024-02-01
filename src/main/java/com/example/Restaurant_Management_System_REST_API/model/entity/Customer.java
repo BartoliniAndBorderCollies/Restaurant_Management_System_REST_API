857913b109dd9d545924d89829b80467750e9ebe
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-02-01 14:31:16 +0100

    add jakarta validations to Customer class

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/Customer.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/Customer.java
@@ -3,6 +3,7 @@ package com.example.Restaurant_Management_System_REST_API.model.entity;
 import com.example.Restaurant_Management_System_REST_API.model.ContactDetails;
 import jakarta.persistence.*;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,15 +35,28 @@ public class Customer implements UserDetails {
     private ContactDetails contactDetails;
 
     //Below are fields for security issues
+    @NotBlank(message = "Password must be set!")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{5,}$", message =
+            "Password must have at least 5 characters, one small letter, one big letter, and a special character")
     private String password;
+
+//            (?=.*[0-9]) ensures there is at least one digit.
+//            (?=.*[a-z]) ensures there is at least one lowercase letter.
+//            (?=.*[A-Z]) ensures there is at least one uppercase letter.
+//            (?=.*[@#$%^&+=]) ensures there is at least one special character.
+//            (?=\\S+$) ensures there are no spaces.
+//.           {5,} ensures there are at least 5 characters.
     private Boolean accountNonExpired;//to avoid hardcoding I established these fields
     private Boolean accountNonLocked;
     private Boolean credentialsNonExpired;
     private Boolean enabled;
+    @NotBlank(message = "Email is missing!")
+    @Email(regexp=".+@.+\\..+", message = "Email address must be as email: example@example.pl")
     private String emailAddress;
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "customer_authorities", joinColumns = @JoinColumn(name = "customer_id"),
             inverseJoinColumns = @JoinColumn(name = "authority_id"))
+    @NotEmpty(message = "Must have at least one authority (role)!")
     private Set<Authority> authorities;
 
     @Override
