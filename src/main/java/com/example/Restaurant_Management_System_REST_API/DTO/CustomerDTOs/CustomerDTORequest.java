e033be41a4f8d1cad998ad052f387dd6e37325c0
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-02-05 19:58:13 +0100

    add @Pattern with regex to validate password for CustomerDTORequest

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/CustomerDTOs/CustomerDTORequest.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/CustomerDTOs/CustomerDTORequest.java
@@ -3,7 +3,7 @@ package com.example.Restaurant_Management_System_REST_API.DTO.CustomerDTOs;
 import com.example.Restaurant_Management_System_REST_API.model.ContactDetails;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Authority;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Reservation;
-import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,7 +24,15 @@ public class CustomerDTORequest {
     private ContactDetails contactDetails;
 
     //Below are fields for security issues
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{5,}$", message =
+            "Password must have at least 5 characters, one digit, one small letter, one big letter, and a special character")
     private String password;
+    //            (?=.*[0-9]) ensures there is at least one digit.
+//            (?=.*[a-z]) ensures there is at least one lowercase letter.
+//            (?=.*[A-Z]) ensures there is at least one uppercase letter.
+//            (?=.*[@#$%^&+=]) ensures there is at least one special character.
+//            (?=\\S+$) ensures there are no spaces.
+//.           {5,} ensures there are at least 5 characters.
     private Boolean accountNonExpired;//to avoid hardcoding I established these fields
     private Boolean accountNonLocked;
     private Boolean credentialsNonExpired;
