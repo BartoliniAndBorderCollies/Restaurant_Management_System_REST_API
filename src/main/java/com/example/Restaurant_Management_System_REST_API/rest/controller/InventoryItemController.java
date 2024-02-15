6c008def52d503d3ea66dabec8270295b6484f4f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-02-15 09:37:29 +0100

    add findAll() in InventoryItemController

--- src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/InventoryItemController.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/InventoryItemController.java
@@ -7,6 +7,8 @@ import com.example.Restaurant_Management_System_REST_API.service.InventoryItemSe
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/inventory/")
 @AllArgsConstructor
@@ -23,6 +25,11 @@ public class InventoryItemController {
     @GetMapping("/find/{id}")
     public InventoryItemDTOResponse findById(@PathVariable Long id) throws NotFoundInDatabaseException {
         return inventoryItemService.findById(id);
+    }
+
+    @GetMapping("/findAll") // This will be done only by owner, manager and staff
+    public List<InventoryItemDTOResponse> findAll() {
+        return inventoryItemService.findAll();
     }
 
 }
