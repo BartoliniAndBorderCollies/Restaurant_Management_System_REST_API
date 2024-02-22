f92b123e9d68554a1d79cdd8f876f7934af09ba3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-02-22 10:33:48 +0100

    add junit test method checking create() and interaction with dependencies

--- src/test/java/com/example/Restaurant_Management_System_REST_API/service/InventoryItemServiceTest.java
+++ src/test/java/com/example/Restaurant_Management_System_REST_API/service/InventoryItemServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.Restaurant_Management_System_REST_API.service;
 
 import com.example.Restaurant_Management_System_REST_API.DTO.InventoryItemDTOs.InventoryItemDTORequest;
+import com.example.Restaurant_Management_System_REST_API.DTO.InventoryItemDTOs.InventoryItemDTOResponse;
 import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
 import com.example.Restaurant_Management_System_REST_API.model.ContactDetails;
+import com.example.Restaurant_Management_System_REST_API.model.entity.InventoryItem;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Supplier;
 import com.example.Restaurant_Management_System_REST_API.repository.InventoryItemRepository;
 import com.example.Restaurant_Management_System_REST_API.repository.SupplierRepository;
@@ -51,6 +53,24 @@ class InventoryItemServiceTest {
         //Act
         //Assert
         assertThrows(NotFoundInDatabaseException.class, ()-> inventoryItemService.create(inventoryItemDTORequest));
+    }
+
+    @Test
+    public void create_ShouldInteractWithDependenciesCorrectly_WhenInventoryItemDTORequestIsGiven() throws NotFoundInDatabaseException {
+        //Arrange
+        InventoryItem inventoryItem = mock(InventoryItem.class);
+        InventoryItemDTORequest inventoryItemDTORequest = mock(InventoryItemDTORequest.class);
+        InventoryItemDTOResponse expected = mock(InventoryItemDTOResponse.class);
+
+        when(modelMapper.map(inventoryItemDTORequest, InventoryItem.class)).thenReturn(inventoryItem);
+        when(inventoryItemRepository.save(inventoryItem)).thenReturn(inventoryItem);
+        when(modelMapper.map(inventoryItem, InventoryItemDTOResponse.class)).thenReturn(expected);
+
+        //Act
+        InventoryItemDTOResponse inventoryItemDTOResponse = inventoryItemService.create(inventoryItemDTORequest);
+
+        //Assert
+        assertEquals(expected, inventoryItemDTOResponse);
     }
 
 }
