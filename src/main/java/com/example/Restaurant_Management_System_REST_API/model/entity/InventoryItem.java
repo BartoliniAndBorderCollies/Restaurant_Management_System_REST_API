44526eeea77b966a5a985a3e21c7db9cc6b4092f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-02-26 14:36:46 +0100

    add constructor to InventoryItem class

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/InventoryItem.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/InventoryItem.java
@@ -13,9 +13,17 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class InventoryItem extends CatalogItem {
+
+    public InventoryItem(Long id, LocalDateTime deliveryDate, int stockAmount, Supplier supplier, String name,
+                         String description, Double price ) {
+        super(name, description, price);
+        this.id = id;
+        this.deliveryDate = deliveryDate;
+        this.stockAmount = stockAmount;
+        this.supplier = supplier;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
