41c11025f609d3994258b0ca7732c62bd38035bc
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-02-29 11:32:13 +0100

    add SupplierRepository

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/SupplierRepository.java
@@ -0,0 +1,7 @@
+package com.example.Restaurant_Management_System_REST_API.repository;
+
+import com.example.Restaurant_Management_System_REST_API.model.entity.Supplier;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SupplierRepository extends CrudRepository<Supplier, Long> {
+}
