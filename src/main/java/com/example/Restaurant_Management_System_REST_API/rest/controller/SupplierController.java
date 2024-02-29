f65552340bc55b901eaa45497977dd4837bea173
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-02-29 11:32:57 +0100

    add SupplierController class and add()

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/SupplierController.java
@@ -0,0 +1,25 @@
+package com.example.Restaurant_Management_System_REST_API.rest.controller;
+
+import com.example.Restaurant_Management_System_REST_API.DTO.SupplierDTOs.SupplierDTORequest;
+import com.example.Restaurant_Management_System_REST_API.DTO.SupplierDTOs.SupplierDTOResponse;
+import com.example.Restaurant_Management_System_REST_API.service.SupplierService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/supplier")
+@AllArgsConstructor
+public class SupplierController {
+
+    private final SupplierService supplierService;
+    
+    //This will be done only by owner, manager and staff
+    @PostMapping("/add")
+    public SupplierDTOResponse add(@RequestBody SupplierDTORequest supplierDTORequest) {
+        return supplierService.add(supplierDTORequest);
+    }
+
+}
