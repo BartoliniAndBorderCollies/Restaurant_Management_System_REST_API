31c03284d9dfc1d81ea43c7406fac3e0f15f0de3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-01 09:28:10 +0100

    add jakarta validations to ContactDetails class

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/ContactDetails.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/ContactDetails.java
@@ -1,6 +1,7 @@
 package com.example.Restaurant_Management_System_REST_API.model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +9,16 @@ import lombok.Setter;
 @Setter
 @Embeddable
 public class ContactDetails {
-
+    @NotBlank(message = "Name must be provided.")
     private String name;
+    @NotBlank(message = "Street must be provided.")
     private String street;
+    @NotBlank(message = "House number must be provided.")
     private String houseNumber;
+    @NotBlank(message = "City must be provided.")
     private String city;
+    @NotBlank(message = "Postal code must be provided.")
     private String postalCode;
+    @NotBlank(message = "Telephone number must be provided.")
     private String telephoneNumber;
 }
