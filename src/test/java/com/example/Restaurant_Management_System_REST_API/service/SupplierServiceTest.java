bffbf4662e60901c760ebda21397a277f6455d02
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-04 08:47:46 +0100

    extract instances from the method to general fields

--- src/test/java/com/example/Restaurant_Management_System_REST_API/service/SupplierServiceTest.java
+++ src/test/java/com/example/Restaurant_Management_System_REST_API/service/SupplierServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.Restaurant_Management_System_REST_API.service;
 
 import com.example.Restaurant_Management_System_REST_API.DTO.SupplierDTOs.SupplierDTORequest;
+import com.example.Restaurant_Management_System_REST_API.DTO.SupplierDTOs.SupplierDTOResponse;
 import com.example.Restaurant_Management_System_REST_API.exception.ObjectAlreadyExistException;
 import com.example.Restaurant_Management_System_REST_API.model.ContactDetails;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Supplier;
@@ -19,29 +20,34 @@ class SupplierServiceTest {
     private SupplierService supplierService;
     private ModelMapper modelMapper;
     private SupplierRepository supplierRepository;
+    private ContactDetails contactDetails;
+    private Supplier supplier;
+    private SupplierDTORequest supplierDTORequest;
+    private SupplierDTOResponse supplierDTOResponse;
 
     @BeforeEach
     public void generalSetUp() {
         modelMapper = mock(ModelMapper.class);
         supplierRepository = mock(SupplierRepository.class);
         supplierService = new SupplierService(supplierRepository, modelMapper);
+
+        contactDetails = new ContactDetails("test name", "test street", "test houseNumber", "test city",
+                "test postalCode", "test telephoneNumber");
+        supplier = new Supplier(1L, contactDetails, null);
+        supplierDTORequest = new SupplierDTORequest(1L, contactDetails, null);
+        supplierDTOResponse = new SupplierDTOResponse(1L, contactDetails, null);
     }
 
     @Test
     public void add_ShouldThrowObjectAlreadyExistException_WhenSupplierAlreadyExist() {
         //Arrange
-        ContactDetails contactDetails = new ContactDetails("test name", "test street", "test houseNumber", "test city",
-                "test postalCode", "test telephoneNumber");
-        Supplier supplier = new Supplier(1L, contactDetails, null);
-        SupplierDTORequest supplierDTORequest = new SupplierDTORequest(1L, contactDetails, null);
-
         when(modelMapper.map(supplierDTORequest, Supplier.class)).thenReturn(supplier);
-       when(supplierRepository.findByContactDetails_NameAndContactDetails_Street(supplier.getContactDetails().getName(),
-                       supplier.getContactDetails().getStreet())).thenReturn(Optional.of(supplier));
+        when(supplierRepository.findByContactDetails_NameAndContactDetails_Street(supplier.getContactDetails().getName(),
+                supplier.getContactDetails().getStreet())).thenReturn(Optional.of(supplier));
 
         //Act
         //Assert
-        assertThrows(ObjectAlreadyExistException.class, ()-> supplierService.add(supplierDTORequest));
+        assertThrows(ObjectAlreadyExistException.class, () -> supplierService.add(supplierDTORequest));
     }
 
 }
