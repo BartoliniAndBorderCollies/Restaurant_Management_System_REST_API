4af4f37de38daa7acc39c92bf3bb3039a78059f3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-06 17:50:00 +0100

    add ReservationController with first method: create()

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/ReservationController.java
@@ -0,0 +1,25 @@
+package com.example.Restaurant_Management_System_REST_API.rest.controller;
+
+import com.example.Restaurant_Management_System_REST_API.DTO.ReservationDTOs.ReservationDTORequest;
+import com.example.Restaurant_Management_System_REST_API.DTO.ReservationDTOs.ReservationDTOResponse;
+import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
+import com.example.Restaurant_Management_System_REST_API.service.ReservationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservation/")
+@AllArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping("/add")
+    public ReservationDTOResponse create(@RequestBody ReservationDTORequest reservationDTORequest)
+            throws NotFoundInDatabaseException {
+        return reservationService.create(reservationDTORequest);
+    }
+}
