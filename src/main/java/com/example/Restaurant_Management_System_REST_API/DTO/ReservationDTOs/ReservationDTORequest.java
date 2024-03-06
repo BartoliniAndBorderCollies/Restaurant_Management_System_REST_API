7fabaa5df746406fe36be4ea2f39cb5a6dcf444f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-06 17:49:26 +0100

    add ReservationDTOs classes

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/ReservationDTOs/ReservationDTORequest.java
@@ -0,0 +1,26 @@
+package com.example.Restaurant_Management_System_REST_API.DTO.ReservationDTOs;
+
+import com.example.Restaurant_Management_System_REST_API.model.entity.Customer;
+import com.example.Restaurant_Management_System_REST_API.model.entity.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationDTORequest {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int peopleAmount;
+    private LocalDateTime start;
+    private ArrayList<Table> tables;
+    private Customer customer;
+}
