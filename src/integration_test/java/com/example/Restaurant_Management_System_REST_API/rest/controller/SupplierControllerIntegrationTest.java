31740c41271d013e61014490388252b68dee3c5b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-06 10:04:02 +0100

    add integration test to check findAll()

--- src/integration_test/java/com/example/Restaurant_Management_System_REST_API/rest/controller/SupplierControllerIntegrationTest.java
+++ src/integration_test/java/com/example/Restaurant_Management_System_REST_API/rest/controller/SupplierControllerIntegrationTest.java
@@ -5,12 +5,14 @@ import com.example.Restaurant_Management_System_REST_API.DTO.SupplierDTOs.Suppli
 import com.example.Restaurant_Management_System_REST_API.model.ContactDetails;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Authority;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Customer;
+import com.example.Restaurant_Management_System_REST_API.model.entity.Supplier;
 import com.example.Restaurant_Management_System_REST_API.repository.AuthorityRepository;
 import com.example.Restaurant_Management_System_REST_API.repository.CustomerRepository;
 import com.example.Restaurant_Management_System_REST_API.repository.SupplierRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -20,6 +22,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -38,6 +41,8 @@ class SupplierControllerIntegrationTest {
     private String basicAuthHeaderOwner;
     @Autowired
     private SupplierRepository supplierRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @BeforeAll
     public void setUpRolesAndCustomers() {
@@ -88,6 +93,34 @@ class SupplierControllerIntegrationTest {
                     assertEquals(expected.getContactDetails().getHouseNumber(), actualResponse.getContactDetails().getHouseNumber());
                     assertEquals(expected.getContactDetails().getPostalCode(), actualResponse.getContactDetails().getPostalCode());
                     assertEquals(expected.getContactDetails().getTelephoneNumber(), actualResponse.getContactDetails().getTelephoneNumber());
+                });
+        supplierRepository.deleteAll();
+    }
+
+    @Test
+    public void findAll_ShouldReturnSupplierDTOResponseList_WhenSupplierExist() {
+        ContactDetails contactDetails = new ContactDetails("test name", "test street", "test houseNumber",
+                "test city", "test postalCode", "test telephoneNumber");
+        Supplier supplier = new Supplier(null, contactDetails, null);
+        supplierRepository.save(supplier);
+
+        SupplierDTOResponse supplierDTOResponse = modelMapper.map(supplier, SupplierDTOResponse.class);
+        supplierDTOResponse.setInventoryItemList(new ArrayList<>()); // set inventoryItemList to an empty list otherwise
+        //there is a difference in assertion (null vs empty list)
+
+        List<SupplierDTOResponse> expected = Arrays.asList(supplierDTOResponse);
+
+        webTestClient.get()
+                .uri("/api/supplier/findAll")
+                .header(HttpHeaders.AUTHORIZATION, basicAuthHeaderOwner)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(SupplierDTOResponse.class)
+                .consumeWith(response -> {
+                    List<SupplierDTOResponse> actualResponse = response.getResponseBody();
+                    assertNotNull(actualResponse);
+                    assertEquals(expected.size(), actualResponse.size());
+                    assertThat(actualResponse).containsExactlyInAnyOrderElementsOf(expected);
                 });
         supplierRepository.deleteAll();
     }
