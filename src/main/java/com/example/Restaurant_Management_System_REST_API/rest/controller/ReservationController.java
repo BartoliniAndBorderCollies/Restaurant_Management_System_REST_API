2bad87c6e3d9c47e3a0a5791d38eabb7dc7a0788
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-08 09:53:08 +0100

    add findById() in ReservationController class

--- src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/ReservationController.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/ReservationController.java
@@ -6,10 +6,7 @@ import com.example.Restaurant_Management_System_REST_API.exception.CustomerAlrea
 import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
 import com.example.Restaurant_Management_System_REST_API.service.ReservationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/reservation/")
@@ -18,9 +15,18 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+    // this will be done by owner, manager and staff
     @PostMapping("/add")
     public ReservationDTOResponse create(@RequestBody ReservationDTORequest reservationDTORequest)
             throws NotFoundInDatabaseException, CustomerAlreadyHasReservationException {
         return reservationService.create(reservationDTORequest);
     }
+
+    // this will be done by owner, manager and staff
+    @GetMapping("/find/{id}")
+    public ReservationDTOResponse findById(@PathVariable Long id) throws NotFoundInDatabaseException {
+        return reservationService.findById(id);
+    }
+
+
 }
