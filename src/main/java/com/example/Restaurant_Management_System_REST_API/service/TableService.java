ec0bb0153de6f4cdcae57d4b0a810a96bc20585f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-20 09:11:03 +0100

    add deleteById() in TableService

--- src/main/java/com/example/Restaurant_Management_System_REST_API/service/TableService.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/service/TableService.java
@@ -1,9 +1,12 @@
 package com.example.Restaurant_Management_System_REST_API.service;
 
+import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Table;
 import com.example.Restaurant_Management_System_REST_API.repository.TableRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,5 +28,13 @@ public class TableService {
         tableRepository.findAll().forEach(table -> tableList.add(modelMapper.map(table, Table.class)));
 
         return tableList;
+    }
+
+    public ResponseEntity<?> deleteById(Long id) throws NotFoundInDatabaseException {
+        Table tableToDelete = tableRepository.findById(id).orElseThrow(()-> new NotFoundInDatabaseException(Table.class));
+
+        tableRepository.delete(tableToDelete);
+
+        return new ResponseEntity<>("Table with id " + tableToDelete.getId() + " has been deleted!", HttpStatus.OK);
     }
 }
