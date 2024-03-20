7e901bb409b9e86b254811eabcb73c0eb5eefd8f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-20 09:00:30 +0100

    rename of field from Order to RestaurantOrder because order is reserved keyword in SQL

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/Table.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/Table.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -18,7 +17,7 @@ public class Table {
     private Long id;
     private boolean isAvailable;
     @OneToMany(mappedBy = "table")
-    private List<Order> orders;
+    private List<RestaurantOrder> restaurantOrders;
     @ManyToOne
     @JoinColumn(name = "reservation_id")
     private Reservation reservation;
