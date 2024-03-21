396f0e474e3e1b700a825144edbb0adaf954febe
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-21 13:25:04 +0100

    add TableDTO class

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
@@ -0,0 +1,10 @@
+package com.example.Restaurant_Management_System_REST_API.DTO.TableDTO;
+
+import com.example.Restaurant_Management_System_REST_API.model.entity.Reservation;
+
+public class TableDTO {
+
+    private Long id;
+    private boolean isAvailable;
+    private Reservation reservation;
+}
