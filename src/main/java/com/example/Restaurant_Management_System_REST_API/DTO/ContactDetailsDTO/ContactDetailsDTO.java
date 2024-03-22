29574489978b0cc9b319cbac216d1bbc8d0b2a69
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-22 10:57:12 +0100

    add @Data to ContactDetailsDTO

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/ContactDetailsDTO/ContactDetailsDTO.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/ContactDetailsDTO/ContactDetailsDTO.java
@@ -1,11 +1,13 @@
 package com.example.Restaurant_Management_System_REST_API.DTO.ContactDetailsDTO;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class ContactDetailsDTO {
 
     private String name;
