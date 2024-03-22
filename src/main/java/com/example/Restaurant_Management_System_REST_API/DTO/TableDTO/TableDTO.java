43caee1f153547b7165126770bf5cbf72b5f33cd
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-22 10:23:15 +0100

    add getter and setter to TableDTO

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
@@ -1,7 +1,11 @@
 package com.example.Restaurant_Management_System_REST_API.DTO.TableDTO;
 
 import com.example.Restaurant_Management_System_REST_API.model.entity.Reservation;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class TableDTO {
 
     private Long id;
