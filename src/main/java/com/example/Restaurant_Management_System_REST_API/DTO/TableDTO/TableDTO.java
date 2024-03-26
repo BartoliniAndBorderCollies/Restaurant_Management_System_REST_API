418ea103e0b668db18e0ce3997c9b7e7c6fd9b82
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-26 09:09:20 +0100

    delete of unneeded fields in TableDTO

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
@@ -1,10 +1,7 @@
 package com.example.Restaurant_Management_System_REST_API.DTO.TableDTO;
 
-import com.example.Restaurant_Management_System_REST_API.model.entity.Reservation;
 
 public class TableDTO {
 
     private Long id;
-    private boolean isAvailable;
-    private Reservation reservation;
 }
