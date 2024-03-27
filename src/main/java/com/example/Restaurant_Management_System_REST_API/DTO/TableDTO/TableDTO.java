ef043fe4e087fb12ad1906b4a660e2476675f894
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-27 10:24:21 +0100

    add @Getter and @Setter to TableDTO

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
@@ -1,6 +1,11 @@
 package com.example.Restaurant_Management_System_REST_API.DTO.TableDTO;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TableDTO {
 
     private Long id;
