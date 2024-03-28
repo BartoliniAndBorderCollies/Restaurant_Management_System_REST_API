dd6d90f2146a033398b6707a7a655028c892341e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-28 12:29:15 +0100

    add @Data to TableDTO to be able to compare instances

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
@@ -1,11 +1,13 @@
 package com.example.Restaurant_Management_System_REST_API.DTO.TableDTO;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class TableDTO {
 
     private Long id;
