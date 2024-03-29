989971b7913408f6bf3a2550c1d0c94880709094
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-03-29 13:20:21 +0100

    add constructors to TableDTO class

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/TableDTO/TableDTO.java
@@ -1,13 +1,13 @@
 package com.example.Restaurant_Management_System_REST_API.DTO.TableDTO;
 
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TableDTO {
 
     private Long id;
