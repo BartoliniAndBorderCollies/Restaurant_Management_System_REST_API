71d24ec996eec037ff852f40b83c00ccd62b638f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-05 14:05:39 +0200

    add RestaurantOrderService with add()

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/service/RestaurantOrderService.java
@@ -0,0 +1,50 @@
+package com.example.Restaurant_Management_System_REST_API.service;
+
+import com.example.Restaurant_Management_System_REST_API.DTO.RestaurantOrderDTOs.RestaurantOrderDTO;
+import com.example.Restaurant_Management_System_REST_API.exception.CustomerAlreadyHasReservationException;
+import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
+import com.example.Restaurant_Management_System_REST_API.model.entity.RestaurantOrder;
+import com.example.Restaurant_Management_System_REST_API.repository.RestaurantOrderRepository;
+import com.example.Restaurant_Management_System_REST_API.service.generic.GenericBasicCrudOperations;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class RestaurantOrderService implements GenericBasicCrudOperations<RestaurantOrderDTO, RestaurantOrderDTO, Long> {
+
+    private final RestaurantOrderRepository restaurantOrderRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public RestaurantOrderDTO create(RestaurantOrderDTO restaurantOrderDTO) throws NotFoundInDatabaseException, CustomerAlreadyHasReservationException {
+        RestaurantOrder restaurantOrder = modelMapper.map(restaurantOrderDTO, RestaurantOrder.class);
+        restaurantOrderRepository.save(restaurantOrder);
+
+        return modelMapper.map(restaurantOrder, RestaurantOrderDTO.class);
+    }
+
+    @Override
+    public RestaurantOrderDTO findById(Long aLong) throws NotFoundInDatabaseException {
+        return null;
+    }
+
+    @Override
+    public List<RestaurantOrderDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public RestaurantOrderDTO update(Long aLong, RestaurantOrderDTO object) throws NotFoundInDatabaseException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> delete(Long aLong) throws NotFoundInDatabaseException {
+        return null;
+    }
+}
