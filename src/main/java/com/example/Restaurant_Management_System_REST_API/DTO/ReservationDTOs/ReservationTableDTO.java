6614d8d7b48207295f9ceb155501a01f5730a52e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-12 10:21:27 +0200

    add ReservationTableDTO class

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/ReservationDTOs/ReservationTableDTO.java
@@ -0,0 +1,11 @@
+package com.example.Restaurant_Management_System_REST_API.DTO.ReservationDTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReservationTableDTO {
+
+    private Long id;
+}
