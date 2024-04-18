c397b27f939c3907f99a3186129186f2cfc963c8
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-18 12:33:37 +0200

    add checkIfMealExist() in RestaurantOrderService

--- src/main/java/com/example/Restaurant_Management_System_REST_API/service/RestaurantOrderService.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/service/RestaurantOrderService.java
@@ -3,6 +3,7 @@ package com.example.Restaurant_Management_System_REST_API.service;
 import com.example.Restaurant_Management_System_REST_API.DTO.RestaurantOrderDTOs.RestaurantOrderDTO;
 import com.example.Restaurant_Management_System_REST_API.exception.CustomerAlreadyHasReservationException;
 import com.example.Restaurant_Management_System_REST_API.exception.NotFoundInDatabaseException;
+import com.example.Restaurant_Management_System_REST_API.model.entity.MenuRecord;
 import com.example.Restaurant_Management_System_REST_API.model.entity.RestaurantOrder;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Table;
 import com.example.Restaurant_Management_System_REST_API.repository.RestaurantOrderRepository;
@@ -24,6 +25,7 @@ public class RestaurantOrderService implements GenericBasicCrudOperations<Restau
     private final RestaurantOrderRepository restaurantOrderRepository;
     private final ModelMapper modelMapper;
     private final TableService tableService;
+    private final MenuRecordService menuRecordService;
 
     @Override
     public RestaurantOrderDTO create(RestaurantOrderDTO restaurantOrderDTO) throws NotFoundInDatabaseException, CustomerAlreadyHasReservationException {
@@ -31,6 +33,18 @@ public class RestaurantOrderService implements GenericBasicCrudOperations<Restau
         restaurantOrderRepository.save(restaurantOrder);
 
         return modelMapper.map(restaurantOrder, RestaurantOrderDTO.class);
+    }
+
+    private void checkIfMealExist(RestaurantOrder restaurantOrder) throws NotFoundInDatabaseException {
+        List<MenuRecord> managedMenuRecords = new ArrayList<>();
+        for (MenuRecord menuRecord : restaurantOrder.getMenuRecords()) {
+            MenuRecord managedMenuRecord = menuRecordService.findByName(menuRecord.getName());
+            //If I don't do the below then these fetched records from line above (from database) flow in the air. They
+            // become managed by the Hibernate session and I cannot save the RestaurantOrder because I got unsaved
+            // transient instance, because they were created outside of this session.
+            managedMenuRecords.add(managedMenuRecord);
+        }
+        restaurantOrder.setMenuRecords(managedMenuRecords);
     }
 
     @Override
