9b8a979139293b227ef0d0a7e87ee495dc3e6504
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-18 12:31:38 +0200

    add MenuRecordForOrderDTO class

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/MenuRecordDTOs/MenuRecordForOrderDTO.java
@@ -0,0 +1,14 @@
+package com.example.Restaurant_Management_System_REST_API.DTO.MenuRecordDTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class MenuRecordForOrderDTO {
+
+    private Long id;
+    private String name;
+
+}
