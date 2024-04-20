92fb8dbffb86a32af25c28636405d26b8ead6f1c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-20 09:53:56 +0200

    add MealIngredient class

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/MealIngredient.java
@@ -0,0 +1,22 @@
+package com.example.Restaurant_Management_System_REST_API.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealIngredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private double amountRequired;
+
+}
