c005fdb11c841e24bd0d669e2f288e056c4b6aad
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-22 13:21:53 +0200

    delete of IngredientRepository.java

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Restaurant_Management_System_REST_API.repository;
-
-import com.example.Restaurant_Management_System_REST_API.model.entity.Ingredient;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
-}
