3833e0acf5e6b1d9e7030e8f4e8511af959c634e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-22 13:22:25 +0200

    add findByName() in InventoryItemRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
@@ -4,6 +4,10 @@ import com.example.Restaurant_Management_System_REST_API.model.entity.InventoryI
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InventoryItemRepository extends CrudRepository<InventoryItem, Long> {
+
+    Optional<InventoryItem> findByName(String name);
 }
