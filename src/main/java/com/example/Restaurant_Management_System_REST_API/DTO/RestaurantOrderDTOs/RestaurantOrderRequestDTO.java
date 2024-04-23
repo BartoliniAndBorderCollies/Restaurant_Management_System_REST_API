4b3b1689dc868e6edff7ac801980d2c52253aa95
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-04-23 14:31:32 +0200

    add RestaurantOrderRequestDTO

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/RestaurantOrderDTOs/RestaurantOrderRequestDTO.java
@@ -0,0 +1,22 @@
+package com.example.Restaurant_Management_System_REST_API.DTO.RestaurantOrderDTOs;
+
+import com.example.Restaurant_Management_System_REST_API.DTO.MenuRecordDTOs.MenuRecordForOrderDTO;
+import com.example.Restaurant_Management_System_REST_API.DTO.TableDTO.TableReservationDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantOrderRequestDTO {
+
+    private TableReservationDTO table;
+    private String telephoneNumber;
+    private List<MenuRecordForOrderDTO> menuRecords;
+}
