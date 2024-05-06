6bbb051ca56a258a1005e71b1edc9ffb71dc7b03
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-06 10:51:51 +0200

    add constructors to RestaurantOrderMenuRecord

--- src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/RestaurantOrderMenuRecord.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/model/entity/RestaurantOrderMenuRecord.java
@@ -1,12 +1,16 @@
 package com.example.Restaurant_Management_System_REST_API.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class RestaurantOrderMenuRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
