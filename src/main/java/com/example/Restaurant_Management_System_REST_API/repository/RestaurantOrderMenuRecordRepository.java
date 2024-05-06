faae344260e158c67fd9e8c77bc0a55c695981f7
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-06 10:50:43 +0200

    move RestaurantOrderMenuRecordRepository to repo package

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/RestaurantOrderMenuRecordRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/RestaurantOrderMenuRecordRepository.java
@@ -1,5 +1,6 @@
-package com.example.Restaurant_Management_System_REST_API.model.entity;
+package com.example.Restaurant_Management_System_REST_API.repository;
 
+import com.example.Restaurant_Management_System_REST_API.model.entity.RestaurantOrderMenuRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantOrderMenuRecordRepository extends JpaRepository<RestaurantOrderMenuRecord, Long> {
