959842f856c4262c9f9085ca1c393ece8916e2bd
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-08 11:50:58 +0200

    refactor of RestaurantOrderMenuRecordService

--- src/main/java/com/example/Restaurant_Management_System_REST_API/service/RestaurantOrderMenuRecordService.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/service/RestaurantOrderMenuRecordService.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,22 +14,10 @@ public class RestaurantOrderMenuRecordService {
 
     private final RestaurantOrderMenuRecordRepository restaurantOrderMenuRecordRepository;
 
-    //this method returns RestaurantOrderMenuRecords with the id of restaurantOrder which is going to be deleted. So this list
-    //which will be returned are restaurantOrderMenuRecords which should be deleted.
-    public List<RestaurantOrderMenuRecord> findRestaurantOrderMenuRecordsForDeletionByRestaurantOrderId(Long restaurantOrderId)  {
-        Iterable<RestaurantOrderMenuRecord> restaurantOrderMenuRecordIterable = restaurantOrderMenuRecordRepository.findAll();
-
-        List<RestaurantOrderMenuRecord> restaurantOrderMenuRecordsToBeDeleted = new ArrayList<>();
-        for (RestaurantOrderMenuRecord eachRestaurantOrderMenuRecord: restaurantOrderMenuRecordIterable) {
-            if(eachRestaurantOrderMenuRecord.getRestaurantOrder().getId().equals(restaurantOrderId)) {
-                restaurantOrderMenuRecordsToBeDeleted.add(eachRestaurantOrderMenuRecord);
-            }
-        }
-        return restaurantOrderMenuRecordsToBeDeleted;
-    }
-
-    public void deleteGivenRestaurantOrderMenuRecords(List<RestaurantOrderMenuRecord> restaurantOrderMenuRecordsToBeDeleted) {
-        for (RestaurantOrderMenuRecord eachRecord: restaurantOrderMenuRecordsToBeDeleted) {
+    public void deleteRestaurantOrderMenuRecordsByRestaurantOrderId(Long restaurantOrderId) {
+        List<RestaurantOrderMenuRecord> restaurantOrderMenuRecordByRestaurantOrderId =
+                restaurantOrderMenuRecordRepository.findRestaurantOrderMenuRecordByRestaurantOrderId(restaurantOrderId);
+        for (RestaurantOrderMenuRecord eachRecord: restaurantOrderMenuRecordByRestaurantOrderId) {
             restaurantOrderMenuRecordRepository.delete(eachRecord);
         }
     }
