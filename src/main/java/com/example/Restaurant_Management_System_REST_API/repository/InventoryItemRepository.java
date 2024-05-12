c2df24c6e9ec6645a70c6a646e05be5fb95a19a4
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-12 11:07:26 +0200

    add methods in InventoryItemRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
@@ -4,10 +4,14 @@ import com.example.Restaurant_Management_System_REST_API.model.entity.InventoryI
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface InventoryItemRepository extends CrudRepository<InventoryItem, Long> {
 
     Optional<InventoryItem> findByName(String name);
+    List<InventoryItem> findByAmountGreaterThan(double amount);
+    List<InventoryItem> findByAmountLessThan(double amount);
+
 }
