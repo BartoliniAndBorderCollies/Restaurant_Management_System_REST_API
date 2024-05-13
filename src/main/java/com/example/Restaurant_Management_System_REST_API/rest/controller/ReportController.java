dbf7e55e175686ac037178898b38ef98af915b4a
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-13 12:36:39 +0200

    add getCustomerByRole() in ReportController

--- src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/ReportController.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/ReportController.java
@@ -1,5 +1,6 @@
 package com.example.Restaurant_Management_System_REST_API.rest.controller;
 
+import com.example.Restaurant_Management_System_REST_API.model.entity.Customer;
 import com.example.Restaurant_Management_System_REST_API.model.entity.InventoryItem;
 import com.example.Restaurant_Management_System_REST_API.service.ReportService;
 import lombok.AllArgsConstructor;
@@ -15,6 +16,8 @@ public class ReportController {
     private final ReportService reportService;
 
     //This will be done by manager and owner only
+
+    //section for InventoryItem reports
     @GetMapping("/inventory/stockAmount/greaterThan")
     public List<InventoryItem> getInventoryItemByAmountGreaterThan(@RequestParam("amount") double amount) {
         return reportService.getInventoryItemByAmountGreaterThan(amount);
@@ -28,6 +31,12 @@ public class ReportController {
     @GetMapping("/inventory/stockAmount/findBySupplier")
     public List<InventoryItem> getInventoryItemBySupplierName(@RequestParam("supplier") String name) {
         return reportService.getInventoryItemBySupplierName(name);
+    }
+
+    //section for Customer reports
+    @GetMapping("/customer/findByRole")
+    public List<Customer> getCustomerByRole(@RequestParam("role") String roleName) {
+        return reportService.getCustomerByRole(roleName);
     }
 
 }
