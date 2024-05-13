5431486c7b2b1710ebf8140074ec99e135c69e0f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-13 12:37:23 +0200

    add getCustomerByRole() in ReportService

--- src/main/java/com/example/Restaurant_Management_System_REST_API/service/ReportService.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/service/ReportService.java
@@ -1,6 +1,8 @@
 package com.example.Restaurant_Management_System_REST_API.service;
 
+import com.example.Restaurant_Management_System_REST_API.model.entity.Customer;
 import com.example.Restaurant_Management_System_REST_API.model.entity.InventoryItem;
+import com.example.Restaurant_Management_System_REST_API.repository.CustomerRepository;
 import com.example.Restaurant_Management_System_REST_API.repository.InventoryItemRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,9 @@ import java.util.List;
 public class ReportService {
 
     private final InventoryItemRepository inventoryItemRepository;
+    private final CustomerRepository customerRepository;
 
+    //section for InventoryItem reports
     public List<InventoryItem> getInventoryItemByAmountGreaterThan(double amount) {
         return inventoryItemRepository.findByAmountGreaterThan(amount);
     }
@@ -24,4 +28,12 @@ public class ReportService {
     public List<InventoryItem> getInventoryItemBySupplierName(String name) {
         return inventoryItemRepository.findBySupplier_ContactDetails_Name(name);
     }
+
+    //section for Customer reports
+    public List<Customer> getCustomerByRole(String roleName) {
+        return customerRepository.findByAuthorities_Authority_name(roleName);
+    }
+
+    //customer with reservations
+
 }
