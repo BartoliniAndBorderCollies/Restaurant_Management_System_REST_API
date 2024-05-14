4d0477b04aa193762f314906ad95706ee4f0a344
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-14 11:00:17 +0200

    add findByIsAvailable() in MenuRecordRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/MenuRecordRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/MenuRecordRepository.java
@@ -4,10 +4,12 @@ import com.example.Restaurant_Management_System_REST_API.model.entity.MenuRecord
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MenuRecordRepository extends CrudRepository<MenuRecord, Long> {
 
     Optional<MenuRecord> findByName(String name);
+    List<MenuRecord> findByIsAvailable(boolean isAvailable);
 }
