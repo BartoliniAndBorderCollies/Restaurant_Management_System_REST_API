ea54c2339fb62892a74e1e3bb804def761829cdc
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-15 08:24:14 +0200

    add findByName() in ReservationRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/ReservationRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/ReservationRepository.java
@@ -3,5 +3,9 @@ package com.example.Restaurant_Management_System_REST_API.repository;
 import com.example.Restaurant_Management_System_REST_API.model.entity.Reservation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+
+    List<Reservation> findByName(String name);
 }
