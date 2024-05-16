ee28da63cbf77d8aec5db92eb3f8f9f562c35d69
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-16 14:41:31 +0200

    add findByStartAndAfter() in ReservationRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/ReservationRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/ReservationRepository.java
@@ -1,8 +1,11 @@
 package com.example.Restaurant_Management_System_REST_API.repository;
 
 import com.example.Restaurant_Management_System_REST_API.model.entity.Reservation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
@@ -10,4 +13,8 @@ public interface ReservationRepository extends CrudRepository<Reservation, Long>
     List<Reservation> findByName(String name);
     List<Reservation> findByPeopleAmountGreaterThan(int peopleAmount);
     List<Reservation> findByPeopleAmountLessThan(int peopleAmount);
+    @Query("SELECT r FROM Reservation r WHERE r.start >= :dateTime")
+    List<Reservation> findByStartAndAfter(@Param("dateTime") LocalDateTime dateTime);
+
+
 }
