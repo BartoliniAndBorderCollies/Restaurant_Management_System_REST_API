b4ca8d2041486aea6650fd3065a2e2c6744da223
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-23 11:00:41 +0200

    add findByOrderTimeRange() with custom Query in RestaurantOrderRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/RestaurantOrderRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/RestaurantOrderRepository.java
@@ -1,9 +1,17 @@
 package com.example.Restaurant_Management_System_REST_API.repository;
 
 import com.example.Restaurant_Management_System_REST_API.model.entity.RestaurantOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface RestaurantOrderRepository extends CrudRepository<RestaurantOrder, Long> {
+
+    @Query("SELECT ro FROM RestaurantOrder ro WHERE ro.orderTime >= :startDateTime AND ro.orderTime < :endDateTime")
+    List<RestaurantOrder> findByOrderTimeRange(@Param("startDateTime") LocalDateTime startDateTime, @Param("endDateTime") LocalDateTime endDateTime);
 }
