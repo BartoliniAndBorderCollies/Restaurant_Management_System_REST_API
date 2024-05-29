4c11cbb904114810a9a43c28e3a60b33a1b2c187
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-05-29 09:01:27 +0200

    add findById() in TableRepository

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/TableRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/TableRepository.java
@@ -4,6 +4,10 @@ import com.example.Restaurant_Management_System_REST_API.model.entity.Table;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TableRepository extends CrudRepository<Table, Long> {
+
+    Optional<Table> findById(Long id);
 }
