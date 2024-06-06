93ebbefa696cc0fa90436877d65975b0b537a423
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-06-06 10:05:28 +0200

    add method to take from repo list of RestaurantOrderMenuRecord in given time period

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/RestaurantOrderMenuRecordRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/RestaurantOrderMenuRecordRepository.java
@@ -1,13 +1,20 @@
 package com.example.Restaurant_Management_System_REST_API.repository;
 
 import com.example.Restaurant_Management_System_REST_API.model.entity.RestaurantOrderMenuRecord;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface RestaurantOrderMenuRecordRepository extends CrudRepository<RestaurantOrderMenuRecord, Long> {
     List<RestaurantOrderMenuRecord> findRestaurantOrderMenuRecordByRestaurantOrderId(Long restaurantOrderId);
+
+    @Query("SELECT romr FROM RestaurantOrderMenuRecord romr WHERE romr.restaurantOrder.orderTime >= :time_from AND romr.restaurantOrder.orderTime < :time_to")
+    List<RestaurantOrderMenuRecord> findRestaurantOrderMenuRecordByTimePeriod(@Param("time_from") LocalDateTime timeFrom, @Param("time_to") LocalDateTime timeTo);
+
 
 }
