4a8fc420261f95dd21e6f503d9d9c4bd478f6c0f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-06-07 09:39:48 +0200

    add new class ResponseDTO

---
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.Restaurant_Management_System_REST_API.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResponseDTO {
+
+    private String message;
+    private HttpStatus status;
+}
