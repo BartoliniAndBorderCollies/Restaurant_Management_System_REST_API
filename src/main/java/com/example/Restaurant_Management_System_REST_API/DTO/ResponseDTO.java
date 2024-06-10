fd533ad0c6590cb26d58e8b126b7144daa9b4363
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-06-10 11:34:17 +0200

    add @Data to ResponseDTO

--- src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/ResponseDTO.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/DTO/ResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.Restaurant_Management_System_REST_API.DTO;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Setter
 @NoArgsConstructor
+@Data
 public class ResponseDTO {
 
     private String message;
