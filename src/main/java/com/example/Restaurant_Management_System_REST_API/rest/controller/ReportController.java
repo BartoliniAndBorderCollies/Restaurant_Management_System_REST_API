e97fc8035c304f958be9741d9db510d7a3752c9b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-06-10 11:58:25 +0200

    move of core logic of creation file to service layer

--- src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/ReportController.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/rest/controller/ReportController.java
@@ -121,41 +121,8 @@ public class ReportController {
             @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
 
-        List<RestaurantOrder> restaurantOrderByOrderTimeRange = reportService.getRestaurantOrderByOrderTimeRange(startDate, endDate);
+        StreamingResponseBody stream = reportService.getRestaurantOrderByOrderTimeRange(startDate, endDate);
 
-        StreamingResponseBody stream = outputStream -> {
-
-            Workbook workbook = new XSSFWorkbook();
-            Sheet sheet = workbook.createSheet("restaurantOrdersByTimeRange");
-
-            // Create header row
-            Row headerRow = sheet.createRow(0);
-            headerRow.createCell(0).setCellValue("Restaurant Order ID");
-            headerRow.createCell(1).setCellValue("Order time");
-            headerRow.createCell(2).setCellValue("Order status");
-            headerRow.createCell(3).setCellValue("Table id");
-            headerRow.createCell(4).setCellValue("Telephone number");
-            headerRow.createCell(5).setCellValue("Total amount to pay");
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-            // Fill data rows
-            for (int i = 0; i < restaurantOrderByOrderTimeRange.size(); i++) {
-                RestaurantOrder restaurantOrder = restaurantOrderByOrderTimeRange.get(i);
-                OrderStatus orderStatus = restaurantOrder.getOrderStatus();
-
-                Row row = sheet.createRow(i + 1);
-                row.createCell(0).setCellValue(restaurantOrder.getId());
-                row.createCell(1).setCellValue(restaurantOrder.getOrderTime().format(formatter));
-                row.createCell(2).setCellValue(orderStatus.toString());
-                row.createCell(3).setCellValue(restaurantOrder.getTable().getId());
-                row.createCell(4).setCellValue(restaurantOrder.getTelephoneNumber());
-                row.createCell(5).setCellValue(restaurantOrder.getTotalAmountToPay());
-
-            }
-            workbook.write(outputStream);
-            workbook.close();
-        };
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Disposition", "attachment; filename=restaurantOrdersByTimeRange.xlsx");
 
