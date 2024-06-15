70dfcf28f1e8ceb6732a1d88fedbab9c866c4f1c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-06-15 14:58:39 +0200

    delete of unused imports

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
@@ -1,9 +1,7 @@
 package com.example.Restaurant_Management_System_REST_API.repository;
 
 import com.example.Restaurant_Management_System_REST_API.model.entity.InventoryItem;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
