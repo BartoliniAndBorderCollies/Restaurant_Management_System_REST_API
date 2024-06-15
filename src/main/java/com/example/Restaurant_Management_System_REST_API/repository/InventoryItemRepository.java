5ea8f474a676207a4d4f9003daefa93793a942d2
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-06-15 14:40:04 +0200

    add new method with custom query to return List of InventoryItems

--- src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
+++ src/main/java/com/example/Restaurant_Management_System_REST_API/repository/InventoryItemRepository.java
@@ -1,7 +1,9 @@
 package com.example.Restaurant_Management_System_REST_API.repository;
 
 import com.example.Restaurant_Management_System_REST_API.model.entity.InventoryItem;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.Optional;
 public interface InventoryItemRepository extends CrudRepository<InventoryItem, Long> {
 
     Optional<InventoryItem> findByName(String name);
+    @Query("SELECT n FROM InventoryItem n WHERE n.name = :name")
+    List<InventoryItem> findByNameReturnList(@Param("name") String name);
     List<InventoryItem> findByAmountGreaterThan(double amount);
     List<InventoryItem> findByAmountLessThan(double amount);
     List<InventoryItem> findBySupplier_ContactDetails_Name(String name);
